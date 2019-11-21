b909ed8d8805fa83f2b6c87cef5d220a168c3810
TechnoTron15 <54597576+TechnoTron15@users.noreply.github.com>
2019-11-21 14:33:57 -0600

    Added some comments

--- frc/robot/subsystems/DriveTrain.java
+++ frc/robot/subsystems/DriveTrain.java
@@ -7,11 +7,19 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.command.Subsystem;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import frc.robot.commands.Teleop; 
+// Command package reference https://first.wpi.edu/FRC/roborio/release/docs/java/edu/wpi/first/wpilibj/command/package-summary.html
+import edu.wpi.first.wpilibj.command.Subsystem; // Defines major components of the robot for use by commands 
+// The wpilibj package has a bunch of classes relating to managing the hardware in the FRC control system in the robot 
+// wpilibj package reference https://first.wpi.edu/FRC/roborio/beta/docs/java/edu/wpi/first/wpilibj/package-summary.html
+import edu.wpi.first.wpilibj.SpeedControllerGroup; // Allows mutliple speed controller objects to be linked together
+// Drive package reference https://first.wpi.edu/FRC/roborio/beta/docs/java/edu/wpi/first/wpilibj/drive/package-summary.html
+import edu.wpi.first.wpilibj.drive.DifferentialDrive; // A class for creating differential drive robots 
+// CTRE Can package reference https://www.ctr-electronics.com/downloads/api/java/html/namespacecom_1_1ctre_1_1phoenix_1_1motorcontrol_1_1can.html
+// Can is a package that includes WPI-compliant set of CTRE motor controls 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX; // A WPI-compliant speed controller class for the Talon SRX motor controller 
+import frc.robot.commands.Teleop; // imports the Teleop.java command class 
+
+/* A subsystem is a major system of the robot. This subsystem handles the wheel/driving mechanisms */ 
 
 /** This subsystem handles matters relating to the movement of the robot **/ 
 public class DriveTrain extends Subsystem {
@@ -24,6 +32,9 @@ public class DriveTrain extends Subsystem {
   WPI_TalonSRX front_right = new WPI_TalonSRX(2);
   // WPI_TalonSRX is an API instance that is the motor control object from ctr-electronics
 
+  /* The regular FRC-supported speedcontrollers are declared using the SpeedController object, but the Talon SRX is seperate from 
+  CTRE, but because it is WPI-compatible, it can still be included in speed controller groups*/ 
+  
   WPI_TalonSRX back_left   = new WPI_TalonSRX(3);
   WPI_TalonSRX back_right  = new WPI_TalonSRX(4);
   SpeedControllerGroup left = new SpeedControllerGroup(front_left, back_left);
